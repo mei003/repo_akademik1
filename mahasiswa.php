5790c22fd393e903f9eac4e922abfd7471335460
mei003 <menovita1205@gmail.com>
2025-03-17 10:41:18 +0700

    Create mahasiswa.php
    Data mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA mahasiswa</title>
+</head>
+<body>
+    <h6>Data Mahasiswa AMp</h6>
+    <hr>
+    <h6>NIM: 33333</h6>
+    <h6>NAMA: mei</h6>
+    <h6>KETUA KELAS</h6>
+    <a href="index.php">Kembali</a>
+</body>
+</html>
